6b68ae205f6251ff97dc6ecb269b82bb9eac135d
kimgura <oli999@naver.com>
2020-11-18 13:30:16 +0900

    Hello 프로젝트 추가됨

---
+++ Hello/src/test/MainClass.java
@@ -0,0 +1,11 @@
+package test;
+
+// MainClass 라는 이름의 클래스 
+public class MainClass {
+	// main 이라는 이름의 메소드(함수)
+	public static void main(String[] args) {
+		
+		System.out.println("Hello, World!");
+		
+	}
+}
---
+++ Hello/src/test/MainClass02.java
@@ -0,0 +1,10 @@
+package test;
+
+public class MainClass02 {
+	// run 했을때 실행의 흐름이 시작되는 특별한 main 메소드 
+	public static void main(String[] args) {
+		System.out.println("하나");
+		System.out.println("두울");
+		System.out.println("세엣");
+	}
+}
